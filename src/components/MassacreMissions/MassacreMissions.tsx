818b6b0cfce4098d828ecf8871f94271443233a7
Ajlanclos <ajlanclos@gmail.com>
2024-09-24 11:33:44 -0500

    calculating single mission kills

--- src/components/MassacreMissions/MassacreMissions.tsx
+++ src/components/MassacreMissions/MassacreMissions.tsx
@@ -5,6 +5,8 @@ import './MassacreMissions.scss';
 function MassacreMission({ missions }: any) {
     const [visibleRowIdx, setVisibleRowIdx]: any = useState(null);
 
+    let prevMissionKills = 0;
+
     const onRowClick = (index: number) => {
         if(visibleRowIdx !== index) {
             setVisibleRowIdx(index);
@@ -26,16 +28,32 @@ function MassacreMission({ missions }: any) {
         return killDifference >= 0 ? `+${killDifference}` : 0;
     }
 
+    const setMissionKillLabel = (totalKills: number, missionKillCount: number, neededKills: number, index: number) => {
+        let missionKills = 0;
+
+        if (totalKills > 0) {
+            const availableKills = totalKills - prevMissionKills;
+    
+            // Allocate kills up to the current mission's required kill count
+            missionKills = Math.min(availableKills, missionKillCount);
+            
+            // Increment prevMissionKills by the amount of kills assigned to this mission
+            prevMissionKills += missionKills;
+        }
+
+        return `${missionKills} / ${missionKillCount} Kills`;
+    };
+
     return (
         <div className="ActiveMissions w-9">
             <ul className="mission-list">
-                <li className="mission-list-header w-full uppercase text-center">
+                <li className="mission-list-header w-full uppercase">
                     <div className="row-item w-4 px-1 text-left">Issued By</div>
-                    <div className="row-item w-3 text-center px-1">Target</div>
-                    <div className="row-item w-2 text-center px-1">Kills</div>
+                    <div className="row-item w-3 px-1">Target</div>
+                    <div className="row-item w-2 px-1">Kills</div>
                     <div className="row-item w-1 text-center px-1">Diff</div>
-                    <div className="row-item w-1 text-center px-1">Mission</div>
-                    <div className="row-item w-1 text-center px-1">Total Earned</div>
+                    <div className="row-item w-2 text-center px-1">Mission Earnings</div>
+                    <div className="row-item w-2 text-center px-1">Total Earnings</div>
                 </li>
                 {missions?.map((data: any, index: number) => (
                     <Fragment key={index}>
@@ -48,18 +66,17 @@ function MassacreMission({ missions }: any) {
                             </div>
                             <div className="row-item w-3 px-1 py-2">{data.targetFaction}</div>
                             <div className="row-item w-2 px-1 py-2">{`${data.kills} / ${data.neededKills}`} <span className="text-accent">({`${data.neededKills - data.kills} Left`})</span></div>
-                            <div className="row-item w-1 px-1 py-2">{calculateDiff(index)}</div>
-                            <div className="row-item w-1 px-1 py-2">{formatCredit(data.missionRewardTotal)}</div>
-                            <div className="row-item w-1 px-1 py-2 text-accent">{formatCredit(data.missionRewardTotal + data.bountyRewardTotal)}</div>
+                            <div className="row-item w-1 px-1 py-2 text-center">{calculateDiff(index)}</div>
+                            <div className="row-item w-2 px-1 py-2 text-center">{formatCredit(data.missionRewardTotal)}</div>
+                            <div className="row-item w-2 px-1 py-2 text-center text-accent">{formatCredit(data.missionRewardTotal + data.bountyRewardTotal)}</div>
                         </li>
                         {data.missions.map((mission: any, missionIdx: any) => (
-            
                                 visibleRowIdx === index && (
                                     <li key={missionIdx} className="row-item-dropdown uppercase">
                                     <div className="row-item custom w-5 px-1 py-2 uppercase">{mission.LocalisedName}</div>
                                     <div className="row-item w-3 px-1 py-2">{mission.DestinationSystem} <i className="fa-solid fa-chevron-right text-xs mx-2" /> {mission.DestinationStation}</div>
-                                    <div className="row-item w-2 px-1 py-2">{`${0} / ${mission.KillCount} Kills`}</div>
-                                    <div className="row-item w-2 px-1 py-2 text-accent">{formatCredit(mission.Reward)}</div>
+                                    <div className="row-item w-2 px-1 py-2 text-center">{setMissionKillLabel(data.kills, mission.KillCount, data.neededKills, index)}</div>
+                                    <div className="row-item w-2 px-1 py-2 text-accent text-center">{formatCredit(mission.Reward)}</div>
                                 </li>
                                 )
                         ))}
